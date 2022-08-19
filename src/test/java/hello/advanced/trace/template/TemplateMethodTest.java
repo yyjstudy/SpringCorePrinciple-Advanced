d4b2ea588f5ced160456d14d7ccf4450949dc3e5
yongjo <yjlee90@suresofttech.com>
2022-08-19 18:29:42 +0900

    템플릿 메서드 패턴과 콜백 패턴-템플릿 메서드 패턴 - 예제3

--- src/test/java/hello/advanced/trace/template/TemplateMethodTest.java
+++ src/test/java/hello/advanced/trace/template/TemplateMethodTest.java
@@ -42,8 +42,29 @@ public class TemplateMethodTest {
     void templateMethodV1() {
         AbstractTemplate template1 = new SubClassLogic1();
         template1.execute();
-        
+
         AbstractTemplate template2 = new SubClassLogic2();
+        template2.execute();
+    }
+
+    @Test
+    void templateMethodV2() {
+        AbstractTemplate template1 = new AbstractTemplate() {
+            @Override
+            protected void call() {
+                log.info("비지니스 로직1 실행");
+            }
+        };
+        log.info("클래스 이름1={}", template1.getClass());
+        template1.execute();
+
+        AbstractTemplate template2 = new AbstractTemplate() {
+            @Override
+            protected void call() {
+                log.info("비지니스 로직2 실행");
+            }
+        };
+        log.info("클래스 이름2={}", template2.getClass());
         template2.execute();
     }
 
